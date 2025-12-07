e7ba24e842c2601b7a477a7355c03f113724d030
1hyphend <Ivanc221@nycstudents.net>
2025-12-06 21:57:34 -0500

    Update test.py

--- test.py
+++ test.py
@@ -1,5 +1,5 @@
 # class pet():
-#     def loompa(x,y):
+#     def __init__(name, self):
 
 
 
@@ -8,16 +8,38 @@
 
 
 
-class Calculator():
-    def add(x, y):
-        print(x + y)
-        return x + y
+# class Calculator():
+#     def add(x, y):
+#         print(x + y)
+#         return x + y
+#     def add_many(numbers):
+#         print(sum(numbers))
+#         return sum(numbers)
+#     def subtract(numbers):
+#         return numbers
+# Calculator.add(5, 67)
 
-    def add_many(numbers):
-        print(sum(numbers))
-        return sum(numbers)
 
-    def subtract(numbers):
-        return numbers
 
-Calculator.add(5, 67)
+
+
+
+period= input("Tell me a period number 3-10: ")
+period=int(period)
+while period:
+    if period==3:
+        print("Mr. H is a very good teacher that helps me when i need it.")
+    elif period==4:
+        print("Mr. Toaha teaches my very important skills that everyone should know.")
+    elif period==5:
+        print("Mr. Babel is a very fun teacher.")
+    elif period==6:
+        print("Lunch is food for the stomach and code is food for the soul.")
+    elif period==7:
+        print("Ms.Bruno teaches me world history that helps me in ELA")
+    elif period==8:
+        print("Mr. Sosa is a fun teacher that always find a way to make the class laugh")
+    elif period==9:
+        print("Mr. Waugh is a very chill teacher")
+    elif period==10:
+        print("Mr.Chiechi teaches us how to speak italian for when we go to Italy")
