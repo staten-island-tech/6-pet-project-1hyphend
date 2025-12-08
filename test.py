4d909b0490993858edfff96f942ba2e9f21a1942
1hyphend <Ivanc221@nycstudents.net>
2025-12-08 10:16:56 -0500

    Update test.py

--- test.py
+++ test.py
@@ -1,19 +1,29 @@
-<<<<<<< HEAD
-# class pet():
-#     def __init__(name, self):
-=======
-class pet():
-    def __init__ (self, name, inventory):
-        self.name = name 
-        self.inventory = inventory 
-    def buy(self, items):
-        self.inventory.append(items)
+class superdupercooltryranasourausrex():
+    def __init__ (self, petname, money, happiness, hunger, clean,name):
+        self.petname = petname
+        self.money = money 
+        self.happiness = happiness
+        self.hunger = hunger
+        self.clean = clean
+        self.name = name
+    def alive(self):
 
 
+    def alive(pet):
 
 
 
+    hunger = 100
+    clean = 100
+    sleep = 100
+    happiness = 100 
+        
 
+    if self.pet
+    hunger > 0
+    sleep > 0 
+    clean > 0 
+    happiness > 0
 
 
 
@@ -30,19 +40,9 @@ class pet():
 
 
 
-# class Calculator():
-#     def add(x, y):
-#         print(x + y)
-#         return x + y
 
-#     def add_many(numbers):
-#         print(sum(numbers))
-#         return sum(numbers)
 
-#     def subtract(numbers):
-#         return numbers
 
-# Calculator.add(5, 67)
 
 
 
@@ -66,14 +66,12 @@ class pet():
 
 
 
-# .lower makes things all letters lower case
 
 
 
 
 
 
->>>>>>> ac8fc1853316b50c0c522094540d01025b95df1e
 
 
 
@@ -82,44 +80,380 @@ class pet():
 
 
 
-<<<<<<< HEAD
-# class Calculator():
-#     def add(x, y):
-#         print(x + y)
-#         return x + y
-#     def add_many(numbers):
-#         print(sum(numbers))
-#         return sum(numbers)
-#     def subtract(numbers):
-#         return numbers
-# Calculator.add(5, 67)
 
 
 
 
 
 
-period= input("Tell me a period number 3-10: ")
-period=int(period)
-while period:
-    if period==3:
-        print("Mr. H is a very good teacher that helps me when i need it.")
-    elif period==4:
-        print("Mr. Toaha teaches my very important skills that everyone should know.")
-    elif period==5:
-        print("Mr. Babel is a very fun teacher.")
-    elif period==6:
-        print("Lunch is food for the stomach and code is food for the soul.")
-    elif period==7:
-        print("Ms.Bruno teaches me world history that helps me in ELA")
-    elif period==8:
-        print("Mr. Sosa is a fun teacher that always find a way to make the class laugh")
-    elif period==9:
-        print("Mr. Waugh is a very chill teacher")
-    elif period==10:
-        print("Mr.Chiechi teaches us how to speak italian for when we go to Italy")
-=======
-
-
-
->>>>>>> ac8fc1853316b50c0c522094540d01025b95df1e
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# make a "COC" api 
+# first we gotta make za troops
+# all troops that cost elixr
+# 1.barb
+# 2.archer
+# 3.goblins
+# 4.e drag
+# 5.e titan
+# 6.wizard
+# 7.balloon 
+# 8.giant
+# 9.bombers
+# 10.
+# Dark elixr troops
+# 1.Valxrye
+# 2.minion
+# 3.golem
+# 4.hog rida
+# 5.witch 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# 😂😂😂😂😂🤣🤣🤣😁😂🤣🤣😁😂🤣🤣
+
+
+
+
+
+
+
+
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# # class Calculator():
+# #     def add(x, y):
+# #         print(x + y)
+# #         return x + y
+
+# #     def add_many(numbers):
+# #         print(sum(numbers))
+# #         return sum(numbers)
+
+# #     def subtract(numbers):
+# #         return numbers
+
+# # Calculator.add(5, 67)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# # .lower makes things all letters lower case
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
