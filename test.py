a957680bea72d1b345517318f643888196eab6ca
1hyphend <Ivanc221@nycstudents.net>
2025-12-11 07:44:02 -0500

    Update test.py

--- test.py
+++ test.py
@@ -1,109 +1,114 @@
 class superdupercooltryranasourausrex:
-    def __init__ (self, petname, money, happiness, hunger, clean, sleep):
+    def __init__(self, petname, money, happiness, hunger, clean, sleep):
         self.petname = petname
         self.money = money 
         self.happiness = happiness
         self.hunger = hunger
         self.clean = clean
         self.sleep = sleep
+
     def alive(self):
         if self.hunger < 1:
             print ("BRO AINT NO WAY HOW DID U LET YOUR PET DIE U BROKIE")
-            return ("dead")
+            return "dead"
         if self.happiness < 1:
             print("YOUR CAT RAN AWAY")
-            return("dead")
+            return "dead"
         if self.clean < 1: 
-            print ("your pet has rabies and bit you so you both DIED HAhA")
-            return("dead")
+            print ("your pet has rabies you DIED HAhA")
+            return "dead"
         if self.sleep < 1:
             print("You shalln't advance thy day bluddy, your pet needs the sleep")
-            return("dead")
-    def eat (self):
+            return "dead"
+
+    def eat(self):
         if self.money >= 25:
-            self.money - 15
-            self.hunger + 20
-            self.happiness + 6
-            print("you fed your pet thumbs up emoji")
-    def sleep (self):
-        if sleep(self): 
-            self.sleep +=20
-            day +=1
-            print("sleeping through the night. IF YOU KNOW YOU KNOW HAHAHAHA")
-    def play (self): 
+            self.money -= 25
+            self.hunger += 15
+            self.happiness += 6
+            print("you fed your pet")
+
+    def sleep_pet(self):
+        self.sleep += 15
+        self.hunger -=15
+        self.clean -=10
+        print("sleeping through the night n\IF YOU KNOW YOU KNOW ")
+
+    def play(self): 
         if self.money >= 10:
-            self.money - 10 
-            self.happiness +15
-            self.hunger - 5
-            self.sleep - 5
-            print("your pet is so happy")
+            self.money -= 10
+            self.happiness += 15
+            self.hunger -= 5
+            self.sleep -= 5
+            print("your pet is happy")
+
     def work(self): 
-        self.money + 35
-        self.happiness - 5
-        self.hunger - 5
-        self.sleep - 5
-        self.clean - 5
-    def shower (self):
-        if self.clean >= 10:
-            self.money - 5
-            self.clean + 10
-            self.happiness +5
-            print("you have cleaned your pet")
+        self.money += 35
+        self.happiness -= 5
+        self.hunger -= 5
+        self.sleep -= 5
+        self.clean -= 5
+
+    def shower(self):
+        if self.money >= 5:
+            self.money -= 5
+            self.clean += 10
+            self.happiness += 5
+            print("you cleaned your pet")
+
     def stats(self):
-        print ("\n heres your current stat")
-        print ("You have this much robux:", self.money)
-        print("You have this much hungry:", self.hunger)
-        print("You have this mich happyness", self.happiness)
-        print("Youhave this much clean", self.clean)
-        print("You have this much sleep:", self.sleep)
-    
+        print("\nHere are your current stats:")
+        print("---------------------------")
+        print("Robux:", self.money)
+        print("\nHunger:", self.hunger)
+        print("\nHappiness:", self.happiness)
+        print("\nClean:", self.clean)
+        print("\nSleep:", self.sleep)
+        print("----------------------------")
+    def money(self):
+        if money >=0:
+            print ("get back to work broke boi")
 
+input("Welcome to raise a flopper!!! Press (((enter))) to start\n")
 
-input("welcome to raise a flopper!!! Here are yoiur stats. \n press enter to advance")
 money = 100
 hunger = 100
 clean = 100
 sleep = 100
-happiness = 100 
-day = 1
+happiness = 100
+name = input("What are you going to name your pet? ")
 
-name = input("what are you going to name your pet")
-pet = superdupercooltryranasourausrex(money, happiness, hunger, clean, sleep)
+pet = superdupercooltryranasourausrex(name, money, happiness, hunger, clean, sleep)
+
 while True:
-    if pet.alive() == False:
+    if pet.alive() == "dead":
         break
 
-print("\n what do you want to do?")
-print("1. ear")
-print("2. play")
-print("3. sleep")
-print ("4. clean")
-print("5. make a youtube video")
+    pet.stats()
 
-choice = input("choose: ")
+    print("\nWhat do you want to do?")
+    print("1. eat")
+    print("2. play")
+    print("3. sleep")
+    print("4. shower")
+    print("5. work")
 
-if choice == "1":
-    pet.eat
-if choice == "2":
-    pet.play
-if choice == "3":
-    pet.sleep
-if choice == "4":
-    pet. clean
-if choice == "5":
-    pet.work
-else:
-    print ("you have to choose an option from 1 to 4")
+    choice = input("Choose: ")
 
-
-
-
-#     HOW DO I DO THIS BRUH 
-#     WoRK ON YHIS TOMOMOROW MORNING 
+    if choice == "1":
+        pet.eat()
+    elif choice == "2":
+        pet.play()
+    elif choice == "3":
+        pet.sleep_pet()
+    elif choice == "4":
+        pet.shower()
+    elif choice == "5":
+        pet.work()
+    else:
+        print("choose an option from 1-5 not 6 or 7")
     
 
-
-# class human(superdupercooltryranasourausrex):
 
 # make a "COC" api 
 # first we gotta make za troops
